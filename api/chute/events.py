805f062910c37d1a48a73fdacb41012f620a836f
Jon Durbin <jon@jondurbin.com>
2024-11-18 08:50:44 -0500

    App code/entrypoint.

--- api/chute/events.py
+++ api/chute/events.py
@@ -3,6 +3,7 @@ Event listeners for chutes.
 """
 
 import uuid
+import hashlib
 from sqlalchemy import event
 from api.chute.schemas import Chute
 
@@ -13,3 +14,11 @@ def generate_uuid(_, __, chute):
     Set chute_id deterministically.
     """
     chute.chute_id = str(uuid.uuid5(uuid.NAMESPACE_OID, f"{chute.user_id}::chute::{chute.name}"))
+
+
+@event.listens_for(Chute, "before_insert")
+def set_version(_, __, chute):
+    """
+    Set the version (sha256 of the code).
+    """
+    chute.version = hashlib.sha256(chute.code.encode()).hexdigest()
