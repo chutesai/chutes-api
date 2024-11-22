6a7f9a9cc7834017b8ba8662c250477cdc01872b
Jon Durbin <jon@jondurbin.com>
2024-11-22 12:06:11 -0500

    Update filesystem challenge, require core libs challenges.

--- data/micro.py
+++ data/micro.py
@@ -3,18 +3,19 @@ from chutes.image import Image
 from chutes.chute import Chute, NodeSelector
 
 image = (
-    Image("micro", "0.0.1")
+    Image(username="test", name="micro", tag="0.0.1")
     .from_base("alpine:latest")
     .run_command("apk add python3 py3-pip shadow && ln -sf $(which python3) /usr/bin/python")
     .run_command("adduser chutes -D /home/chutes")
-    .run_command("mkdir -p /home/chutes && chown chutes:chutes /home/chutes")
+    .run_command("mkdir -p /app /home/chutes && chown chutes:chutes /home/chutes /app")
     .set_user("chutes")
-    .set_workdir("/home/chutes")
+    .set_workdir("/app")
     .run_command("python -m venv venv")
-    .with_env("PATH", "/home/chutes/venv/bin:$PATH")
+    .with_env("PATH", "/app/venv/bin:$PATH")
 )
 
 chute = Chute(
+    username="test",
     name="micro",
     image=image,
     node_selector=NodeSelector(
