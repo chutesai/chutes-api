9afa812e9f819c021298361259d36952ab9d96f6
David Smith <david@corcel.io>
2024-11-29 15:50:09 +0000

    fix a bunch of issues with underlying infra and pin versions

--- helm/install.sh
+++ helm/install.sh
@@ -4,18 +4,20 @@ set -euo pipefail
 # install repos, make sure they're up to date
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo add grafana https://grafana.github.io/helm-charts
+helm repo add hashicorp https://helm.releases.hashicorp.com
 helm repo update
 
-# setup log aggregation, metrics, and observability
-helm upgrade --install observability grafana/loki-stack --set grafana.enabled=true,prometheus.enabled=true
+# if you need it, setup log aggregation, metrics, and observability
+helm upgrade --install observability grafana/loki-stack --version 2.10.2 --values values_observability.yaml
 
-# install postgres
-helm upgrade --install postgres oci://registry-1.docker.io/bitnamicharts/postgresql --values values_postgres.yaml
+# if you need it, install postgres
+helm upgrade --install postgres oci://registry-1.docker.io/bitnamicharts/postgresql --version 16.2.3 --values values_postgres.yaml
 
-# install redis
-helm upgrade --install redis oci://registry-1.docker.io/bitnamicharts/redis --values values_redis.yaml
+#if you need it,  install redis
+helm upgrade --install redis oci://registry-1.docker.io/bitnamicharts/redis --version 20.3.0 --values values_redis.yaml
 
-# install minio
-helm upgrade --install redis oci://registry-1.docker.io/bitnamicharts/minio --values values_minio.yaml
+#if you need it,  install minio
+helm upgrade --install minio oci://registry-1.docker.io/bitnamicharts/minio --version 14.8.5 --values values_minio.yaml
 
-# install registry
+#if you need it,  install vault
+helm upgrade --install vault oci://registry-1.docker.io/bitnamicharts/vault --version 1.4.30 --values values_vault.yaml
