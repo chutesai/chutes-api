03b38771917e7afbed1e221838180cb23b58e077
Jon Durbin <jon@jondurbin.com>
2025-01-10 23:58:19 +0000

    Update instance selection.

--- api/instance/util.py
+++ api/instance/util.py
@@ -11,7 +11,7 @@ from api.config import settings
 from api.database import get_session
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
-from sqlalchemy import func, case, text
+from sqlalchemy import case, text
 from sqlalchemy.orm import aliased
 from datetime import datetime
 
@@ -21,27 +21,14 @@ InstanceAlias = aliased(Instance)
 
 async def discover_chute_targets(session: AsyncSession, chute_id: str, max_wait: int = 0):
     """
-    Fancy query to attempt evenly distributing queries based on coldkey and
-    last invocation timestamp.
+    Evenly distribute queries to miners.
     """
-    subquery = (
-        select(InstanceAlias.miner_coldkey, func.count().label("instance_count"))
-        .where(InstanceAlias.active.is_(True))
-        .where(InstanceAlias.verified.is_(True))
-        .where(InstanceAlias.chute_id == chute_id)
-        .group_by(InstanceAlias.miner_coldkey)
-        .subquery()
-    )
-
-    # Main query to fetch instances filtered by chute_id and ordered as needed
     query = (
         select(Instance)
-        .join(subquery, Instance.miner_coldkey == subquery.c.miner_coldkey)
         .where(Instance.active.is_(True))
         .where(Instance.verified.is_(True))
         .where(Instance.chute_id == chute_id)
         .order_by(
-            subquery.c.instance_count,
             case(
                 (Instance.last_queried_at.is_(None), datetime.min), else_=Instance.last_queried_at
             ).asc(),
