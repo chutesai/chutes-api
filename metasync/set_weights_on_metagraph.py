5e5362d8962892185541003f5fc710a1c6d76bf9
Jon Durbin <jon@jondurbin.com>
2025-01-13 06:23:42 -0500

    Scoring updates (#17)
    - Use median tokens per second per chute normalized compute time for vllm to normalize compute time
    - Use median steps per second per chute normalized compute time for diffusion
    - Use additional metrics in score calculation:
      - 45% weight on total amount of compute time (compute muliplier * total time)
      - 25% on total number of invocations (meaning they handle a lot of requests)
      - 20% on unique chute count, meaning they aren't just running a single/small number of chutes
      - 10% on bounty count, bounty values help the compute time, this rewards being good at getting bounties, regardless of compute time

--- metasync/set_weights_on_metagraph.py
+++ metasync/set_weights_on_metagraph.py
@@ -20,6 +20,55 @@ from fiber.chain.chain_utils import query_substrate
 VERSION_KEY = 69420  # Doesn't matter too much in chutes' case
 logger = get_logger(__name__)
 
+# Proportion of weights to assign to each metric.
+FEATURE_WEIGHTS = {
+    "compute_units": 0.45,      # Total amount of compute time (compute muliplier * total time).
+    "invocation_count": 0.25,   # Total number of invocations.
+    "unique_chute_count": 0.2,  # Number of unique chutes over the scoring period.
+    "bounty_count": 0.1,        # Number of bounties received (not bounty values, just counts).
+}
+SCORING_INTERVAL = "7 days"
+NORMALIZED_COMPUTE_QUERY = """
+WITH computation_rates AS (
+    SELECT
+        chute_id,
+        percentile_cont(0.5) WITHIN GROUP (ORDER BY extract(epoch from completed_at - started_at) / (metrics->>'steps')::float) as median_step_time,
+        percentile_cont(0.5) WITHIN GROUP (ORDER BY extract(epoch from completed_at - started_at) / (metrics->>'tokens')::float) as median_token_time
+    FROM invocations
+    WHERE (metrics->>'steps' IS NOT NULL and (metrics->>'steps')::float > 0)
+       OR (metrics->>'tokens' IS NOT NULL and (metrics->>'tokens')::float > 0)
+    GROUP BY chute_id
+)
+SELECT
+    i.miner_hotkey,
+    COUNT(*) as invocation_count,
+    COUNT(DISTINCT(i.chute_id)) AS unique_chute_count,
+    COUNT(CASE WHEN i.bounty > 0 THEN 1 END) AS bounty_count,
+    sum(
+        i.bounty +
+        i.compute_multiplier *
+        CASE
+            WHEN i.metrics->>'steps' IS NOT NULL
+                AND r.median_step_time IS NOT NULL
+                AND EXTRACT(EPOCH FROM (i.completed_at - i.started_at)) > ((i.metrics->>'steps')::float * r.median_step_time)
+            THEN (i.metrics->>'steps')::float * r.median_step_time
+            WHEN i.metrics->>'tokens' IS NOT NULL
+                AND r.median_token_time IS NOT NULL
+                AND EXTRACT(EPOCH FROM (i.completed_at - i.started_at)) > ((i.metrics->>'tokens')::float * r.median_token_time)
+            THEN (i.metrics->>'tokens')::float * r.median_token_time
+            ELSE EXTRACT(EPOCH FROM (i.completed_at - i.started_at))
+        END
+    ) AS compute_units
+FROM invocations i
+LEFT JOIN computation_rates r ON i.chute_id = r.chute_id
+WHERE i.started_at > NOW() - INTERVAL '{interval}'
+AND i.error_message IS NULL
+AND i.miner_uid > 0
+AND i.completed_at IS NOT NULL
+GROUP BY i.miner_hotkey
+ORDER BY compute_units DESC;
+"""
+
 
 async def _get_validator_node_id(
     substrate: SubstrateInterface, netuid: int, ss58_address: str
@@ -34,47 +83,61 @@ async def _get_weights_to_set(
     hotkeys_to_node_ids: dict[str, int],
 ) -> tuple[list[int], list[float]] | None:
     """
-    Naive v1; Sum compute time for non errored invocations multiplied by compute multiplier.
+    Query the invocations for the past {SCORING INTERVAL} to calculate weights.
 
-    IMPROVEMENTS:
-    - Factor in cold starts
+    Factors included in scoring are:
+    - total compute time provided (as a factor of compute multiplier PLUS bounties awarded)
+    - total number of invocations processed
+    - number of unique chutes executed
+    - number of bounties claimed
+
+    Future improvements:
     - Punish errors more than just ignoring them
     - Have a decaying, normalised reward, rather than a fixed window
-    - Reward for time registered on the network (capped)
     """
 
-    interval = "7 days"
-
-    query = text(
-        f"""
-        SELECT
-            i.miner_hotkey,
-            sum(i.bounty + i.compute_multiplier * EXTRACT(EPOCH FROM (i.completed_at - i.started_at))) AS compute_units
-        FROM invocations i
-        WHERE i.started_at > NOW() - INTERVAL '{interval}'
-        AND i.error_message IS NULL
-        AND miner_uid > 0
-        GROUP BY i.miner_hotkey
-        HAVING SUM(i.bounty + i.compute_multiplier * EXTRACT(EPOCH FROM (i.completed_at - i.started_at))) > 0
-        """
-    )
-
-    miner_compute_units = {}
+    query = text(NORMALIZED_COMPUTE_QUERY.format(interval=SCORING_INTERVAL))
+    raw_compute_values = {}
+    header = ["hotkey", "invocation_count", "unique_chute_count", "bounty_count", "compute_units"]
     async with get_session() as session:
         result = await session.execute(query)
-        for miner_hotkey, compute_units in result:
-            logger.info(f"Compute units for {miner_hotkey=} = {compute_units}")
-            miner_compute_units[miner_hotkey] = compute_units
+        for row in result:
+            obj = dict(zip(header, row))
+            raw_compute_values[obj["hotkey"]] = obj
+            logger.info(obj)
 
+    # Normalize the values based on totals so they are all in the range [0.0, 1.0]
+    totals = {
+        key: sum(row[key] for row in raw_compute_values.values()) or 1.0
+        for key in header[1:]
+    }
+    normalized_values = {
+        hotkey: {
+            key: row[key] / totals[key]
+            for key in header[1:]
+        }
+        for hotkey, row in raw_compute_values.items()
+    }
+    # Adjust the values by the feature weights, e.g. compute_time gets more weight than bounty count.
+    final_scores = {
+        hotkey: sum(
+            norm_value * FEATURE_WEIGHTS[key]
+            for key, norm_value in metrics.items()
+        )
+        for hotkey, metrics in normalized_values.items()
+    }
+
+    # Final weights per node.
     node_ids = []
     node_weights = []
-    for hotkey, compute_units in miner_compute_units.items():
+    for hotkey, compute_score in final_scores.items():
         if hotkey not in hotkeys_to_node_ids:
             logger.debug(f"Miner {hotkey} not found on metagraph. Ignoring.")
             continue
 
-        node_weights.append(compute_units)
+        node_weights.append(compute_score)
         node_ids.append(hotkeys_to_node_ids[hotkey])
+        logger.info(f"Normalized score for {hotkey}: {compute_score}")
 
     return node_ids, node_weights
 
