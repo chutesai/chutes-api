41c434cf985fc67401544745e18fb7b83b5bf219
Jon Durbin <jon@jondurbin.com>
2025-03-06 10:15:13 +0000

    Remove graval services (completely separate infra/services now), allow toggling some services on/off.

--- api/config/__init__.py
+++ api/config/__init__.py
@@ -129,7 +129,7 @@ class Settings(BaseSettings):
     ip_rate_limit_window: int = int(os.getenv("IP_RATE_LIMIT_WINDOW", 60))
 
     # Chutes pinned version.
-    chutes_version: str = os.getenv("CHUTES_VERSION", "0.2.20")
+    chutes_version: str = os.getenv("CHUTES_VERSION", "0.2.21")
 
     # Auto stake amount when DCAing into alpha after receiving payments.
     autostake_amount: float = float(os.getenv("AUTOSTAKE_AMOUNT", "0.03"))
