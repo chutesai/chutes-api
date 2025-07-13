3b8719a7b69ee16ba50bff6044c9515de084c612
Jon Durbin <jon@jondurbin.com>
2025-07-13 13:19:28 +0000

    Skip secrets scan in trivy.

--- data/trivy_scan.sh
+++ data/trivy_scan.sh
@@ -9,4 +9,4 @@ target_container=$(buildah from $target_image)
 mountpoint=$(buildah mount $target_container)
 
 # Scan.
-trivy fs --severity HIGH,CRITICAL --ignore-unfixed $mountpoint
+trivy fs --severity HIGH,CRITICAL --scanners vuln --ignore-unfixed $mountpoint
