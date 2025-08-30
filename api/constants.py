159b9192b0155d9f9f1d4324baa5dba6dab9e082
Jon Durbin <jon@jondurbin.com>
2025-08-30 10:54:10 +0000

    Price adjustments.

--- api/constants.py
+++ api/constants.py
@@ -15,20 +15,23 @@ ENCRYPTED_HEADER = "X-Chutes-Encrypted"
 # per million really can change depending on the node selector.
 # For example:
 #  llama-3-8b with node selector requiring minimally an h100
-#  Example h100 hourly price (subject to change): $1.15
-#  $/million = $1.15 * 0.016447 = $0.01891405/million input
-#            = $1.15 * 0.065785 = $0.07565275/million output
+#  Example h100 hourly price (subject to change): $1.5
+#  $/million = $1.5 * 0.01358695 = $0.02/million input
+#            = $1.5 * 0.05434782 = $0.08/million output
 # Deepseek example, 8x h200:
-#  $1.9 * 8 * 0.016447 = $0.25/million input
-#  $1.9 * 8 * 0.065785 = $1.00/million output
+#  $2.3 * 8 * 0.01358695 = $0.25/million input
+#  $2.3 * 8 * 0.05434782 = $1.00/million output
 # NOTE: there is also a multiplier when the chute's concurrency is < 16,
 # because for example the concurrency may be reduced to accomodate more
 # total concurrent tokens in KV cache, such as GLM-4.5-FP8 at full context
 # has concurrency 12, so:
-#  $1.9 * 8 * 16/12 * 0.016447 = $0.33/million input
-#  $1.9 * 8 * 16/12 * 0.065785 = $1.33/million output
-LLM_PRICE_MULT_PER_MILLION_IN = 0.016447
-LLM_PRICE_MULT_PER_MILLION_OUT = 0.065785
+#  $2.3 * 8 * 16/12 * 0.01358695 = $0.33/million input
+#  $2.3 * 8 * 16/12 * 0.05434782 = $1.33/million output
+# Kimi-K2 example (8xb200)
+#  $3.5 * 8 * 0.01358695 = $0.38
+#  $3.5 * 8 * 0.05434782 = $1.52
+LLM_PRICE_MULT_PER_MILLION_IN = 0.01358695
+LLM_PRICE_MULT_PER_MILLION_OUT = 0.05434782
 LLM_MIN_PRICE_IN = 0.01
 LLM_MIN_PRICE_OUT = 0.01
 
