45c0e870649241f3f3d14392e1e850a24e21b200
Jon Durbin <jon@jondurbin.com>
2025-10-08 12:38:33 +0000

    Fix auth.

--- api/chute/router.py
+++ api/chute/router.py
@@ -757,9 +757,10 @@ async def get_chute(
     authorized = False
     if chute:
         if (
-            chute.user_id == current_user.user_id
-            or await is_shared(chute.chute_id, current_user.user_id)
-            or subnet_role_accessible(chute, current_user, admin=True)
+            (current_user and chute.user_id == current_user.user_id)
+            or (current_user and await is_shared(chute.chute_id, current_user.user_id))
+            or (current_user and subnet_role_accessible(chute, current_user, admin=True))
+            or "affine" in chute.name.lower()
         ):
             authorized = True
     if not authorized:
